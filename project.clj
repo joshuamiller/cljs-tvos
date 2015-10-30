6abc1ef61b7b64413422943764a7ca09e66e3e8e
Joshua Miller <josh@joshmiller.io>
2015-10-30 10:21:07 -0700

    Add license to project.clj.

--- project.clj
+++ project.clj
@@ -1,6 +1,10 @@
 (defproject cljs-tvos "0.1.0"
   :description "ClojureScript bindings for AppleTV's TVML and TVJS"
   :url "https://github.com/joshuamiller/cljs-tvos"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.145" :classifier "aot"
                   :exclusion [org.clojure/data.json]]
